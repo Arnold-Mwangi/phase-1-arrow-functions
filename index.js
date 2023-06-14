ccb3aedee55ba929cebe69a2c76f10da328a2b4c
Arnold-Mwangi <kirigwimwangi18@yahoo.com>
2023-06-14 17:12:56 +0300

    completed Assignment

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = () => (2000/100);
+console.log(divide)
+
+const square = (num1) => (num1 * num1);
+console.log(square)
+
+const add = (num1, num2) => (num1 + num2);
+console.log(add);
